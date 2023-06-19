74b6c13e994eac769fec00727aa40c3aa8262a4d
Vishal Vinay Ram <vishalvinayram5432@gmail.com>
2023-06-19 21:52:05 +0530

    before check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import BasicExample from "./navbar";
 export default function MyComponent() {
   const [formData, setFormData] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  const [bool,setBool] = useState(false);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setFormData(event.target.value);
@@ -39,10 +40,11 @@ export default function MyComponent() {
                   width: "100%",
                   padding: "2% 1% 1% 1%",
                   margin: "4px 0 0px 30px",
-                  border: "3px solid red",
+                  border: "3px solid GREY",
                   borderRadius: "12px",
+                  textAlign:"center"
                 }}
-              >
+              >   <h4 style={styles.small_heading}>Step 1</h4>
                 <div
                   style={{
                     height: "60%",
@@ -58,13 +60,17 @@ export default function MyComponent() {
                     boxShadow: "0px 2px 4px soli",
                   }}
                 >
+               
                   {imageUrl ? (
+                    <div>
+                                                <input type='checkbox' />
                     <img
                       src={imageUrl}
                       alt="Uploaded Image"
                       width="100%"
                       height="100%"
                     />
+                    </div>
                   ) : (
                     <p>No image uploaded</p>
                   )}
@@ -84,36 +90,65 @@ export default function MyComponent() {
                 </Col>
               </div>
             </Col>
-            <Col sm={12}>
-              <div
-                style={{
-                  height: "100%",
-                  width: "100%",
-                  minWidth: "250px",
-                  minHeight: "250px",
-                  marginTop: "5%",
-                  border: "1px solid grey",
-                  display: "flex",
-                  margin: "10px 10px 10px 10px",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  borderRadius: "4px",
-                  boxShadow: "0px 2px 4px soli",
-                }}
-              >
-                <div>
-                  
-                  <div>
-                    <input
-                      type="text"
-                      value={formData}
-                      onChange={handleInputChange}
+            <Col sm={8}>
+              
+                <div style={{
+                  padding:"2% 2% 2% 2%",
+                  margin:"5px 5px 5px 5px",
+                  border:"1px solid black",
+                  width:"fit-content",
+                  borderRadius:"5px"
+                }}>
+                                 
+
+                
+                    <div
                       style={styles.inputs}
+                    ><h4 style={styles.small_heading}>Step2</h4>
+                      <h3 style={styles.main_headinf}>Select the product image from this</h3>
+                       <Col>
+                       {imageUrl ? (
+                        <div>
+                    <img
+                      src={imageUrl}
+                      alt="Uploaded Image"
+                      style={styles.smallimage}
                     />
-                  </div>
-                  <img src="/images.jpeg" style={styles.smallimage} />
+                    </div>
+                  ) : (
+                    <p></p>
+                  )}
+                                        {imageUrl ? (
+                    <img
+                      src={imageUrl}
+                      alt="Uploaded Image"
+                      style={styles.smallimage}
+                    />
+                  ) : (
+                    <p></p>
+                  )}
+                                        </Col>
+                      </div>
+                    <div style={{marginLeft:"20px",marginTop:"10px",marginBottom:"10px"}}>
+                    <h4 style={styles.small_heading}>Step 3</h4>
+                    <h2 style={styles.main_headinf}> Select descriptiotn for the event</h2>
+                    </div>
+                   <div style={styles.description} >Id amet sunt proident non eiusmod aliquip exercitation consequat. Nostrud veniam exercitation et reprehenderit mollit. Reprehenderit eu proident excepteur elit aliqua duis est pariatur cupidatat enim anim esse dolore. Mollit occaecat culpa ut esse cillum minim id do sunt incididunt. Ea culpa non eiusmod incididunt sunt adipisicing veniam sint ex.</div>
+                   <div style={styles.description}>Id amet sunt proident non eiusmod aliquip exercitation consequat. Nostrud veniam exercitation et reprehenderit mollit. Reprehenderit eu proident excepteur elit aliqua duis est pariatur cupidatat enim anim esse dolore. Mollit occaecat culpa ut esse cillum minim id do sunt incididunt. Ea culpa non eiusmod incididunt sunt adipisicing veniam sint ex.</div>
+                    
+
+          <div>
+          {imageUrl ? (
+                    <img
+                      src={imageUrl}
+                      style={styles.descimage}
+                    />
+                  ) : (
+                    <p>+</p>
+                  )}
+          </div>
+                  
                 </div>
-              </div>
             </Col>
           </Row>
         </Container>
@@ -123,6 +158,34 @@ export default function MyComponent() {
 }
 
 const styles = {
+  description:
+  {border:"2px solid blue",fontSize:"8px",color:"black",padding:"4px 4px 4px 4px",
+  borderRadius:"5px"
+},
+  contiainer_border:
+  {
+    backgroundColor:"white",
+    width: "100%",
+    height:"20px",
+    padding: "2% 1% 1% 1%",
+    margin: "4px 0 0px 30px",
+    border: "3px solid GREY",
+    borderRadius: "12px",
+    textAlign:"center"
+  } as React.CSSProperties,
+  main_headinf:
+  {
+    fontSize:"1rem",color:"grey"
+  },
+  small_heading:
+  {
+    marginTop:"5px",
+    marginBottom:"5px",
+    marginRight:"5px",
+        fontSize:"8px",
+    color:"grey",
+    fontWeight:"400"
+  },
   container: {
     position: "relative",
     height: "100vh",
@@ -130,24 +193,47 @@ const styles = {
     alignItems: "center",
     justifyContent: "center",
     backgroundImage: "url('/5566879.jpg')",
-    backgroundRepeat: "no-repeat",
+    backgroundRepeat: "4",
     backgroundSize: "cover",
     padding: "20px",
     boxShadow: "0px 2px 4px black",
   } as React.CSSProperties,
   smallimage: {
+    marginLeft:"10px",
+    marginRight:"10px",
     height: "30px",
     width: "30px",
+  },
+  descimage: {
+    marginTop:"5px",
+    marginBottom:"5px",
+    maxHeight:"150px",
+        marginLeft:"5px",
+    marginRight:"5px",
+    height: "100%",
+    width: "100%",
   },
   input: {
     backgroundColor: "#F1F1F1",
     borderRadius: "8px",
     boxShadow: "0px 2px 4px black",
     width: "50%",
-    height: "30px",
+    height: "20px",
     padding: "8px",
     margin: "2px 10px 5px 10px",
-    marginBottom: "16px",
+    marginLeft: "2%",
+    fontSize: "16px",
+    color: "#333333",
+    border: "1px solid #CCCCCC",
+    outline: "none",
+  },
+  inputing: {
+    backgroundColor: "#F1F1F1",
+    borderRadius: "8px",
+    boxShadow: "0px 2px 4px black",
+    height: "10vh",
+    padding: "8px",
+    marginBottom: "5px",
     fontSize: "16px",
     color: "#333333",
     border: "1px solid #CCCCCC",
@@ -157,12 +243,11 @@ const styles = {
     backgroundColor: "#F1F1F1",
     borderRadius: "8px",
     boxShadow: "0px 2px 4px black",
-    width: "100%",
-    height: "20vh",
-
+    height: "15vh",
     padding: "8px",
-    marginBottom: "16px",
-    fontSize: "16px",
+    marginRight:"2%",
+    marginLeft: "2%",
+    fontSize: "1em",
     color: "#333333",
     border: "1px solid #CCCCCC",
     outline: "none",
